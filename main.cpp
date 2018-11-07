f235cfe443c53e227704a7a43594f59a1121a9e9
Camila Leite da Silva <camila.leite002@gmail.com>
2018-11-07 03:21:15 -0200

    Update main.cpp
    Aritmetical functions are working, apparently. It is missing the conditionals functions and the calls.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,171 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-vector<string> single_parameter_functions;
-vector<string> double_parameter_functions;
+vector<string> parameter_functions;
+vector<string> aritmetical_functions;
+vector<string> conditional_functions;
 vector<string> parameterless_functions;
 
 //Initiate the possible program commands
 void fill_parameters() {
 
-	single_parameter_functions.push_back("CALL");
-	single_parameter_functions.push_back("RET");
+	parameter_functions.push_back("CALL");
+	parameter_functions.push_back("RET");
 
-	double_parameter_functions.push_back("MOV");
-	double_parameter_functions.push_back("ADD");
-	double_parameter_functions.push_back("SUB");
-	double_parameter_functions.push_back("MUL");
-	double_parameter_functions.push_back("DIV");
-	double_parameter_functions.push_back("MOD");
-	double_parameter_functions.push_back("IFEQ");
-	double_parameter_functions.push_back("IFNEQ");
-	double_parameter_functions.push_back("IFG");
-	double_parameter_functions.push_back("IFL");
-	double_parameter_functions.push_back("IFGE");
-	double_parameter_functions.push_back("IFLE");
+	aritmetical_functions.push_back("MOV");
+	aritmetical_functions.push_back("ADD");
+	aritmetical_functions.push_back("SUB");
+	aritmetical_functions.push_back("MUL");
+	aritmetical_functions.push_back("DIV");
+	aritmetical_functions.push_back("MOD");
+
+	conditional_functions.push_back("IFEQ");
+	conditional_functions.push_back("IFNEQ");
+	conditional_functions.push_back("IFG");
+	conditional_functions.push_back("IFL");
+	conditional_functions.push_back("IFGE");
+	conditional_functions.push_back("IFLE");
 
 	parameterless_functions.push_back("ENDIF");
 
 }
 
+vector<string> global_variables;
+
+//Initiate the possible program commands
+void fill_variables() {
+
+	global_variables.push_back("R0");
+	global_variables.push_back("R1");
+	global_variables.push_back("R2");
+	global_variables.push_back("R3");
+	global_variables.push_back("R4");
+	global_variables.push_back("R5");
+	global_variables.push_back("R6");
+	global_variables.push_back("R7");
+	global_variables.push_back("R8");
+	global_variables.push_back("R9");
+
+}
+
+map<string, int> fill_variables(int n) {
+
+	map<string, int> local_variables;
+	local_variables.insert(make_pair("R0", n));
+	local_variables.insert(make_pair("R1", 0));
+	local_variables.insert(make_pair("R2", 0));
+	local_variables.insert(make_pair("R3", 0));
+	local_variables.insert(make_pair("R4", 0));
+	local_variables.insert(make_pair("R5", 0));
+	local_variables.insert(make_pair("R6", 0));
+	local_variables.insert(make_pair("R7", 0));
+	local_variables.insert(make_pair("R8", 0));
+	local_variables.insert(make_pair("R9", 0));
+	return local_variables;
+
+}
+
+//Execute the command to associate b to a.
+int comand_mov(int a, int b) {
+
+	return b;
+}
+
+//Executing the command of addition
+int comand_add(int a, int b) {
+
+	return a + b;
+}
+
+//Executing the command of subtraction
+int comand_sub(int a, int b) {
+
+	return a - b;
+}
+
+//Executing the command of multiplication
+int comand_mul(int a, int b) {
+
+	return a * b;
+}
+
+//Executing the command of division
+int comand_div(int a, int b) {
+
+	if (b == 0)
+		return 0;
+	return (a/b);
+}
+
+//Executing the command of mod
+int comand_mod(int a, int b) {
+
+	return a % b;
+}
+
+//Executing the command of ifeq
+bool comand_ifeq(int a, int b) {
+
+	if (a == b)
+		return true;
+	else
+		return false;
+
+}
+
+//Executing the command of ifneq
+bool comand_ifneq(int a, int b) {
+
+	if (a != b)
+		return true;
+	else
+		return false;
+
+}
+
+//Executing the command of ifg
+bool comand_ifg(int a, int b) {
+
+	if (a > b)
+		return true;
+	else
+		return false;
+
+}
+
+//Executing the command of ifl
+bool comand_ifl(int a, int b) {
+
+	if (a < b)
+		return true;
+	else
+		return false;
+
+}
+
+//Executing the command of ifge
+bool comand_ifge(int a, int b) {
+
+	if (a >= b)
+		return true;
+	else
+		return false;
+
+}
+
+//Executing the command of ifle
+bool comand_ifle(int a, int b) {
+
+	if (a <= b)
+		return true;
+	else
+		return false;
+
+}
 
 //Function that splits a string, 
 //return a pair with the first and second splitted part
@@ -71,24 +206,102 @@ pair<string, string> getLine() {
 
 }
 
+int RunTwoParametersFunction(string function_name, map<string, int> local_variables, string a, int var2) {
+
+	map<string, int>::iterator it;
+
+	it = local_variables.find(a);
+	int var1 = it->second;
+	   
+	if (function_name.compare("MOV") == 0)
+		it->second = comand_mov(var1, var2);
+
+	else if(function_name.compare("ADD") == 0)
+		it->second = comand_add(var1, var2);
+
+	else if (function_name.compare("SUB") == 0)
+		it->second = comand_sub(var1, var2);
+
+	else if (function_name.compare("MUL") == 0)
+		it->second = comand_mul(var1, var2);
+
+	else if (function_name.compare("DIV") == 0)
+		it->second = comand_div(var1, var2);
+
+	else if (function_name.compare("MOD") == 0)
+		it->second = comand_mod(var1, var2);
+
+	return it->second;
+
+}
+
+//str1.compare(str2) != 0)
+void runProgram(vector<pair<string, string>> program_lines, int n) {
+
+	map<string, int> local_variables;
+	local_variables = fill_variables(n);
+	map<string, int>::iterator it;
+
+	for (int i = 0; i < program_lines.size(); i++) {
+
+		cout << program_lines.at(i).first << " ";
+
+		if (find(aritmetical_functions.begin(), aritmetical_functions.end(), program_lines.at(i).first) != aritmetical_functions.end()) {
+			
+			cout << program_lines.at(i).first << " ";
+			
+			pair<string, string> function_variables = splitString(program_lines.at(i).second, ",");
+			
+			it = local_variables.find(function_variables.second);
+			int var2;
+			if (it != local_variables.end())
+				var2 = it->second;
+			else
+				var2 = stoi(function_variables.second);
+
+			it = local_variables.find(function_variables.first);
+			
+			cout << function_variables.first << " " << function_variables.second << " ";
+			
+			it->second = RunTwoParametersFunction(program_lines.at(i).first, local_variables, function_variables.first, var2);
+			
+			cout << it->first<<"="<<it->second<<endl;
+		
+		}
+		else if (program_lines.at(i).first.compare("RET") == 0) {
+
+			it = local_variables.find(program_lines.at(i).second);
+			int aux = it->second;
+			it = local_variables.find("R9");
+			it->second = aux;
+			cout << "RETORNA:" << aux;
+
+		}
+		else {
+			cout << "nao tratado ainda" << endl;
+		}
+
+	}
+
+}
+
 void getIteration() {
 
-	pair<string, string> line = getLine();
-	int l, n;
-	l = stoi(line.first);
-	n = stoi(line.second);
-	vector<pair<string, string>> program_lines;
+	int l = 1, n = 1;
 
-	while (l != 0 && n != 0){
+	while (l != 0 && n != 0) {
+
+		pair<string, string> line = getLine();
+		l = stoi(line.first);
+		n = stoi(line.second);
+		vector<pair<string, string>> program_lines;
 
 		for (int i = 0; i < l; i++)
 			program_lines.push_back(getLine());
 
-
+		runProgram(program_lines, n);
 
 	}
-
-	cout << "Oi";
 }
 
 
@@ -97,6 +310,7 @@ void getIteration() {
 int main() {
 
 	fill_parameters();
+	fill_variables();
 	getIteration();
 
 	return 0;
