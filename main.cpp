9c1db915536670d460a70026f4d6782b5046cfab
Camila Leite da Silva <camila.leite002@gmail.com>
2018-11-08 19:02:01 -0200

    Update main.cpp
    ACEITOOOO

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-/ HaltingProblem.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
 #include <string>
 #include <iostream>
 #include <vector>
@@ -8,27 +5,13 @@
 
 using namespace std;
 
+int counter = 0;
+
 vector<string> aritmetical_functions;
 vector<string> conditional_functions;
-
-//Initiate the possible program commands
-void fill_parameters() {
-
-	aritmetical_functions.push_back("MOV");
-	aritmetical_functions.push_back("ADD");
-	aritmetical_functions.push_back("SUB");
-	aritmetical_functions.push_back("MUL");
-	aritmetical_functions.push_back("DIV");
-	aritmetical_functions.push_back("MOD");
-
-	conditional_functions.push_back("IFEQ");
-	conditional_functions.push_back("IFNEQ");
-	conditional_functions.push_back("IFG");
-	conditional_functions.push_back("IFL");
-	conditional_functions.push_back("IFGE");
-	conditional_functions.push_back("IFLE");
-
-}
+vector<vector<pair<string, string>>> saved_programs;
+vector<map<int, int>> saved_pre_computed_solutions;
+map<int, int> pre_computed;
 
 //Set a value to each variable.
 map<string, int> fill_local_variables(int n) {
@@ -55,9 +38,19 @@ int comand_mov(int a, int b) {
 
 }
 
+//Executing the command of mod
+int comand_mod(int a, int b) {
+
+	if (b == 0)
+		return 0;
+	return a % b;
+}
+
 //Executing the command of addition
 int comand_add(int a, int b) {
 
+	if (a + b > 999)
+		return comand_mod(a + b, 1000);
 	return a + b;
 
 }
@@ -65,13 +58,21 @@ int comand_add(int a, int b) {
 //Executing the command of subtraction
 int comand_sub(int a, int b) {
 
+	if(a-b<0)
+		return 1000 + (a - b);
 	return a - b;
 }
 
 //Executing the command of multiplication
 int comand_mul(int a, int b) {
 
-	return a * b;
+	int aux = b;
+	if (a == 0 || b == 0)
+		return 0;
+	else
+		for (int i = 1; i < a; i++)
+			b = comand_add(b, aux);
+	return b;
 }
 
 //Executing the command of division
@@ -79,15 +80,8 @@ int comand_div(int a, int b) {
 
 	if (b == 0)
 		return 0;
+
 	return (a / b);
-}
-
-//Executing the command of mod
-int comand_mod(int a, int b) {
-
-	if (b == 0)
-		return 0;
-	return a % b;
 }
 
 //Executing the command of ifeq
@@ -179,16 +173,20 @@ pair<string, string> getLine() {
 //Check if a given function is aritmetical or not
 bool checkIfAritmetical(string s) {
 
-	bool aux = (find(aritmetical_functions.begin(), aritmetical_functions.end(), s) != aritmetical_functions.end());
-	return aux;
+	if (s.compare("MOV") != 0 && s.compare("ADD") != 0 && s.compare("SUB") != 0 && s.compare("MUL") != 0 && s.compare("DIV") != 0 && s.compare("MOD") != 0)
+		return false;
+	else
+		return true;
 
 }
 
 //Check if a given function is conditional or not
 bool checkIfConditional(string s) {
 
-	bool aux = (find(conditional_functions.begin(), conditional_functions.end(), s) != conditional_functions.end());
-	return aux;
+	if (s.compare("IFEQ") != 0 && s.compare("IFNEQ") != 0 && s.compare("IFG") != 0 && s.compare("IFL") != 0 && s.compare("IFGE") != 0 && s.compare("IFLE") != 0)
+		return false;
+	else
+		return true;
 
 }
 
@@ -266,59 +264,82 @@ bool runConditionals(string function_name, map<string, int> local_variables, int
 //Controls the program execution
 int runProgram(vector<pair<string, string>> program_lines, int n) {
 
-	map<string, int> local_variables;
-	local_variables = fill_local_variables(n);
-	map<string, int>::iterator it;
+	if (counter == 800) {
 
-	//check if the function already entered in an if function.
-	//If it is not suppose to execute the subsequent lines of an if, it is false.
-	bool if_check = true;
+		return 1000;
+	}
 
-	for (int i = 0; i < program_lines.size(); i++) {
-	
-		string command_name = program_lines.at(i).first;
-		string command_parameter = program_lines.at(i).second;
-		string variable;
-		int var1, var2;
-
-		if (command_parameter.find(',') != string::npos){
-
-			pair<string, string> function_parameters = splitString(program_lines.at(i).second, ",");
-			variable = function_parameters.first;
-			var1 = getNumber(local_variables, function_parameters.first);
-			var2 = getNumber(local_variables, function_parameters.second);
+	else {
 		
+		counter++;
+		map<string, int> local_variables;
+		local_variables = fill_local_variables(n);
+		map<string, int>::iterator it;
+
+		//check if the function already entered in an if function.
+		//If it is not suppose to execute the subsequent lines of an if, it is false.
+		bool if_check = true;
+
+		for (int i = 0; i < program_lines.size(); i++) {
+
+			string command_name = program_lines.at(i).first;
+			string command_parameter = program_lines.at(i).second;
+			string variable;
+			int var1, var2;
+
+			if (command_parameter.find(',') != string::npos && if_check) {
+
+				pair<string, string> function_parameters = splitString(program_lines.at(i).second, ",");
+				variable = function_parameters.first;
+				var1 = getNumber(local_variables, function_parameters.first);
+				var2 = getNumber(local_variables, function_parameters.second);
+
+			}
+			else if (command_name.compare("ENDIF") && if_check) {
+				variable = command_parameter;
+				var1 = getNumber(local_variables, command_parameter);
+
+			}
+
+			if (checkIfAritmetical(command_name) && if_check) {
+				it = local_variables.find(variable);
+				it->second = RunAritmeticalFunctions(program_lines.at(i).first, local_variables, var1, var2);
+
+			}
+
+			//If it enters in the if, following subsequent the program lines, if it does not, it looks for the next endif.
+			//cout << program_lines.at(i).first << " ";
+			else if (checkIfConditional(command_name) && if_check)
+				if_check = runConditionals(program_lines.at(i).first, local_variables, var1, var2);
+
+			else if (!command_name.compare("ENDIF")) {
+
+				if_check = true;
+			}
+
+			else if (!command_name.compare("RET") && if_check) {
+				pre_computed.insert(make_pair(n, var1));
+				return var1;
+			}
+
+			else if (!command_name.compare("CALL") && if_check) {
+
+				map<int, int>::iterator it_pre_computed;
+				it = local_variables.find("R9");
+				it_pre_computed = pre_computed.find(var1);
+
+				if (it_pre_computed != pre_computed.end())
+					it->second = it_pre_computed->second;
+				else
+					it->second = runProgram(program_lines, var1);
+
+				if (it->second == 1000)
+					return 1000;
+
+			}
+
 		}
-		else if (command_name.compare("ENDIF")) {
-			variable = command_parameter;
-			var1 = getNumber(local_variables, command_parameter);
 		
-		}
-
-		if (checkIfAritmetical(command_name) && if_check) {
-			it = local_variables.find(variable);
-			it->second = RunAritmeticalFunctions(program_lines.at(i).first, local_variables, var1, var2);
-
-		}
-
-		//If it enters in the if, following subsequent the program lines, if it does not, it looks for the next endif.
-		//cout << program_lines.at(i).first << " ";
-		else if (checkIfConditional(program_lines.at(i).first) && if_check)
-			if_check = runConditionals(program_lines.at(i).first, local_variables, var1, var2);
-
-		else if (!command_name.compare("ENDIF"))
-			if_check = true;
-
-		else if (!command_name.compare("RET") && if_check)
-			return var1;
-
-		else if (!command_name.compare("CALL") && if_check) {
-
-			it = local_variables.find("R9");
-			it->second = runProgram(program_lines, var1);
-
-		}
-
 	}
 
 }
@@ -338,13 +359,40 @@ void getIteration() {
 		for (int i = 0; i < l; i++)
 			program_lines.push_back(getLine());
 
-		int i = runProgram(program_lines, n);
-		cout << i << endl;
+		bool existent_program = false;
+		int j;
 
+		for (int i = 0; i < saved_programs.size(); i++) {
+			if (program_lines == saved_programs.at(i)) {
+				j = i;
+				existent_program = true;
+				pre_computed = saved_pre_computed_solutions.at(i);
+				break;
+			}
+		}
+
+		int i = runProgram(program_lines, n);
+		if (i == 1000)
+			cout << "*" << endl;
+		else
+			cout << i << endl;
+
+		counter = 0;
+		
+		if (!existent_program) {
+			
+			saved_pre_computed_solutions.push_back(pre_computed);
+			saved_programs.push_back(program_lines);
+
+		}
+		else if (existent_program) {
+			saved_pre_computed_solutions.at(j) = pre_computed;
+		}
 		pair<string, string> line = getLine();
 		l = stoi(line.first);
 		n = stoi(line.second);
 		program_lines.clear();
+		pre_computed.clear();
 
 	}
 }
@@ -354,7 +402,6 @@ void getIteration() {
 // and not in this file
 int main() {
 
-	fill_parameters();
 	getIteration();
 
 	return 0;
